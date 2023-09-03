89222853c23fd62fa2076cc3dc2ec5d75bebbb67
Júlia Quitério <juliapquiterio@gmail.com>
2023-09-03 22:34:06 +0000

    update anotacao

--- src/main/java/br/gov/sp/fatec/springboot3labiv/entity/Anotacao.java
+++ src/main/java/br/gov/sp/fatec/springboot3labiv/entity/Anotacao.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -39,4 +38,31 @@ public class Anotacao {
 
     public void setId(Long id) {
         this.id = id;
-    }}
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    
+}
