f002ce08ff7f3c237fd68d16483e505e8a0229f7
Júlia Quitério <juliapquiterio@gmail.com>
2023-09-03 22:35:46 +0000

    update usuario repository

--- src/main/java/br/gov/sp/fatec/springboot3labiv/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/springboot3labiv/repository/UsuarioRepository.java
@@ -12,16 +12,15 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     public Optional<Usuario> findByNome(String nome);
 
-	//aqui eu utilizo o nome da classe
+    //aqui eu utilizo o nome da classe
     //ele sempre faz catch
     @Query("select u from Usuario u where u.nome = ?1")
     public Optional<Usuario> buscarPeloNome(String nome);
 
-	//use o contains que eh igual ao like
+    //use o contains que eh igual ao like
     //quando eh para texto eu uso o ignorecase maiusculo ou minusculo
     public List<Usuario> findByNomeContainsIgnoreCase(String nome);
 
-	//buscarpelonome retorna 1 unico, se voltar apenas 1 não use lista
     @Query("select u from Usuario u where u.nome like %?1%")
     public List<Usuario> buscarPeloNomeParecido(String nome);
 
@@ -30,12 +29,13 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     @Query("select u from Usuario u where u.nome = ?1 and u.senha = ?2")
     public Optional<Usuario> buscarPeloNomeESenha(String nome, String senha);
 
-//use autorizacao
+    //use autorizacao
     //ele faz um join
     //se eu usar uma outra condição ele considera com a tabela na qual eu estou
     public List<Usuario> findByAutorizacoesNome(String nomeAutorizacao);
 
-//se quero join eu uso o atributo nçao a tabela em si
+
+    //se quero join eu uso o atributo nçao a tabela em si
     @Query("select u from Usuario u join u.autorizacoes a where a.nome = ?1")
     public List<Usuario> buscarPeloNomeAutorizacao(String nomeAutorizacao);
     
